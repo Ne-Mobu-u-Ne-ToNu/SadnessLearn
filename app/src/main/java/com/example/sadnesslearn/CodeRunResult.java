d7b5367c4cf68c9e8a6feba9833ada4ea76611d4
Ne-MoBu-u-Ne-ToNu <seriy.sery2014@gmail.com>
2022-12-05 14:56:39 +0300

    added Empty codeRunActivity

---
+++ app/src/main/java/com/example/sadnesslearn/CodeRunResult.java
@@ -0,0 +1,26 @@
+package com.example.sadnesslearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Objects;
+
+public class CodeRunResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code_run_result);
+
+        Toolbar tlb_code_run = findViewById(R.id.tlb_code_run);
+        setSupportActionBar(tlb_code_run);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        String s = "Some text";
+        TextView tv_run_code = findViewById(R.id.tv_run_code);
+        tv_run_code.setText(s);
+    }
+}
--- app/src/main/java/com/example/sadnesslearn/Sandbox.java
+++ app/src/main/java/com/example/sadnesslearn/Sandbox.java
@@ -3,8 +3,12 @@ package com.example.sadnesslearn;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.ImageButton;
+
 import com.amrdeveloper.codeview.CodeView;
 import com.example.sadnesslearn.classes.JavaSyntaxManager;
 import java.util.Objects;
@@ -20,6 +24,12 @@ public class Sandbox extends AppCompatActivity {
         Toolbar tlb_sandBox = findViewById(R.id.tlb_sandBox);
         setSupportActionBar(tlb_sandBox);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        ImageButton ib_sanbox_run = findViewById(R.id.ib_sanbox_run);
+        ib_sanbox_run.setOnClickListener(view -> {
+            startActivity(new Intent(Sandbox.this, CodeRunResult.class));
+            overridePendingTransition(R.anim.right_in, R.anim.left_out);
+        });
 
         CodeView code = findViewById(R.id.cv_sandbox);
         JavaSyntaxManager.applyJavaCodeTheme(this, code);
